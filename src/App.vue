a2a43b6a5238dc69e3420a73100fd6706f4a23fa
Neil Nand <neilnand@gmail.com>
2018-03-01 00:45:36 +0800

    Sass and Pug installed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  div(id="app")
+    router-view
+    nav
+      ul
+        li
+          router-link(to="/template") Template
+        li
+          router-link(to="/golden-blockchain") Golden Blockchain
 </template>
 
 <script>
@@ -11,13 +15,19 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" rel="stylesheet/scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
+
+    nav {
+      position: absolute;
+      bottom: 10px;
+      left: 10px;
+    }
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Template from '@/components/Template'
+import GoldenBlockchain from '@/components/GoldenBlockchain'
 
 Vue.use(Router)
 
@@ -8,8 +9,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GoldenBlockchain',
+      component: GoldenBlockchain
+    },
+    {
+      path: '/golden-blockchain',
+      name: 'GoldenBlockchain',
+      component: GoldenBlockchain
+    },
+    {
+      path: '/template',
+      name: 'Template',
+      component: Template
     }
   ]
 })
