c36afd50083cf97ebbeac3454e3a4cd4781af11f
Neil Nand <neilnand@gmail.com>
2018-03-01 23:46:09 +0800

    nodes technique 1

--- src/components/GoldenBlockchain.vue
+++ src/components/GoldenBlockchain.vue
@@ -5,18 +5,65 @@
 
 <script>
 
+class Point {
+  constructor (x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+function createPointNode (ctx, pt, nodes, depth, spread, ptArray) {
+  for (var i = 0; i < nodes; i++) {
+    ctx.moveTo(pt.x, pt.y)
+    var r = spread + Math.random() * spread * 0.8
+    var shift = i / nodes + Math.random() * 0.1
+    var x = r * 1.6 * Math.cos(2 * Math.PI * shift)
+    var y = r * Math.sin(2 * Math.PI * shift)
+    var nPt = new Point(pt.x + x, pt.y + y)
+    ptArray.push(nPt)
+    ctx.lineTo(nPt.x, nPt.y)
+
+    if (depth > 0) {
+      createPointNode(ctx, nPt, 3, depth - 1, spread * 0.8, ptArray)
+    }
+  }
+  depth--
+}
+
+function createNodeCircles (ctx, ptArray) {
+
+}
+
 export default {
   mounted () {
     const canvas = this.$refs.canvas
-    const w = canvas.offsetWidth
-    const h = canvas.offsetHeight
+    const w = canvas.offsetWidth << 1
+    const h = canvas.offsetHeight << 1
 
     var ctx = canvas.getContext('2d')
-    var gradient = ctx.createLinearGradient(0, 0, 1, 0)
-    gradient.addColorStop(0, 'green')
-    gradient.addColorStop(1, 'white')
+    ctx.canvas.width = w
+    ctx.canvas.height = h
+
+    var gradient = ctx.createLinearGradient(0, 0, w, h)
+    gradient.addColorStop(0, '#E7A10E')
+    gradient.addColorStop(1, '#F5D123')
+
+    var gradient2 = ctx.createLinearGradient(0, 0, w, h)
+    gradient2.addColorStop(0, '#F5D123')
+    gradient2.addColorStop(1, '#E7A10E')
+
     ctx.fillStyle = gradient
     ctx.fillRect(0, 0, w, h)
+
+    var ptArray = [new Point(w >> 1, h >> 1)]
+
+    ctx.beginPath()
+    var depth = 3
+    createPointNode(ctx, ptArray[0], 3, depth, 200, ptArray)
+    ctx.lineWidth = 1
+    ctx.strokeStyle = gradient2
+    ctx.strokeStyle = '#ff0000'
+    ctx.stroke()
   }
 }
 </script>
--- src/components/PixiJSDemo.vue
+++ src/components/PixiJSDemo.vue
@@ -5,95 +5,95 @@
 </template>
 
 <script>
-  import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js'
 
-  function getProviders () {
-    return {
-      // These need to be contained in an object because providers are not reactive.
-      PIXIWrapper: {
-        // Expose PIXI and the created app to all descendants.
-        PIXI,
-        PIXIApp: null
-      }
+function getProviders () {
+  return {
+    // These need to be contained in an object because providers are not reactive.
+    PIXIWrapper: {
+      // Expose PIXI and the created app to all descendants.
+      PIXI,
+      PIXIApp: null
     }
   }
+}
 
-  export default {
-    provide: getProviders,
-    inject: ['PIXIWrapper', 'EventBus'],
+export default {
+  provide: getProviders,
+  inject: ['PIXIWrapper', 'EventBus'],
 
-    mounted () {
-      const bgCanvas = this.$refs.bgCanvas
-      const renderCanvas = this.$refs.renderCanvas
-      const w = renderCanvas.offsetWidth
-      const h = renderCanvas.offsetHeight
+  mounted () {
+    const bgCanvas = this.$refs.bgCanvas
+    const renderCanvas = this.$refs.renderCanvas
+    const w = renderCanvas.offsetWidth
+    const h = renderCanvas.offsetHeight
 
-      var ctx = bgCanvas.getContext('2d')
-      var gradient = ctx.createLinearGradient(0, 0, 200, 0)
-      gradient.addColorStop(0, 'green')
-      gradient.addColorStop(1, 'white')
-      ctx.fillStyle = gradient
-      ctx.fillRect(0, 0, 10, 10)
+    var ctx = bgCanvas.getContext('2d')
+    var gradient = ctx.createLinearGradient(0, 0, 200, 0)
+    gradient.addColorStop(0, 'green')
+    gradient.addColorStop(1, 'white')
+    ctx.fillStyle = gradient
+    ctx.fillRect(0, 0, 10, 10)
 
-      var app = new PIXI.Application(w, h, {
-        view: renderCanvas,
-        transparent: true
-      })
+    var app = new PIXI.Application(w, h, {
+      view: renderCanvas,
+      transparent: true
+    })
 
-      var container = new PIXI.Container()
-      container.scale.x = 1
-      container.scale.y = 1
+    var container = new PIXI.Container()
+    container.scale.x = 1
+    container.scale.y = 1
 
-      var graphics = new PIXI.Graphics()
+    var graphics = new PIXI.Graphics()
 
-      // set a fill and line style
-      graphics.beginFill(0xFF3300)
-      graphics.lineStyle(10, 0xffd900, 1)
+    // set a fill and line style
+    graphics.beginFill(0xFF3300)
+    graphics.lineStyle(10, 0xffd900, 1)
 
-      // draw a shape
-      graphics.moveTo(50, 50)
-      graphics.lineTo(250, 50)
-      graphics.lineTo(100, 100)
-      graphics.lineTo(250, 220)
-      graphics.lineTo(50, 220)
-      graphics.lineTo(50, 50)
-      graphics.endFill()
+    // draw a shape
+    graphics.moveTo(50, 50)
+    graphics.lineTo(250, 50)
+    graphics.lineTo(100, 100)
+    graphics.lineTo(250, 220)
+    graphics.lineTo(50, 220)
+    graphics.lineTo(50, 50)
+    graphics.endFill()
 
-      // set a fill and line style again
-      graphics.lineStyle(10, 0xFF0000, 0.8)
-      graphics.beginFill(0xFF700B, 1)
+    // set a fill and line style again
+    graphics.lineStyle(10, 0xFF0000, 0.8)
+    graphics.beginFill(0xFF700B, 1)
 
-      // draw a second shape
-      graphics.moveTo(210, 300)
-      graphics.lineTo(450, 320)
-      graphics.lineTo(570, 350)
-      graphics.quadraticCurveTo(600, 0, 480, 100)
-      graphics.lineTo(330, 120)
-      graphics.lineTo(410, 200)
-      graphics.lineTo(210, 300)
-      graphics.endFill()
+    // draw a second shape
+    graphics.moveTo(210, 300)
+    graphics.lineTo(450, 320)
+    graphics.lineTo(570, 350)
+    graphics.quadraticCurveTo(600, 0, 480, 100)
+    graphics.lineTo(330, 120)
+    graphics.lineTo(410, 200)
+    graphics.lineTo(210, 300)
+    graphics.endFill()
 
-      // draw a rectangle
-      graphics.lineStyle(2, 0x0000FF, 1)
-      graphics.drawRect(50, 250, 100, 100)
+    // draw a rectangle
+    graphics.lineStyle(2, 0x0000FF, 1)
+    graphics.drawRect(50, 250, 100, 100)
 
-      // draw a circle
-      graphics.lineStyle(0)
-      graphics.beginFill(0xFFFF0B, 0.5)
-      graphics.drawCircle(470, 200, 100)
-      graphics.endFill()
+    // draw a circle
+    graphics.lineStyle(0)
+    graphics.beginFill(0xFFFF0B, 0.5)
+    graphics.drawCircle(470, 200, 100)
+    graphics.endFill()
 
-      graphics.lineStyle(20, 0x33FF00)
-      graphics.moveTo(30, 30)
-      graphics.lineTo(600, 300)
+    graphics.lineStyle(20, 0x33FF00)
+    graphics.moveTo(30, 30)
+    graphics.lineTo(600, 300)
 
-      // Add Elements
-      container.addChild(graphics)
-      app.stage.addChild(container)
+    // Add Elements
+    container.addChild(graphics)
+    app.stage.addChild(container)
 
-      this.PIXIWrapper.PIXIApp = app
-    }
+    this.PIXIWrapper.PIXIApp = app
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
