211fb6021e9d7c25bb17d4e58269f6ee6d378b7e
Neil Nand <neilnand@gmail.com>
2018-04-07 22:46:44 +0800

    drawing mechanics updated

--- src/components/GoldenBlockchain.vue
+++ src/components/GoldenBlockchain.vue
@@ -13,29 +13,27 @@ class Point {
   }
 }
 
-function createPointNode (ctx, pt, nodes, depth, spread, ptArray) {
-  for (var i = 0; i < nodes; i++) {
+function createPointNode (countStart, ctx, pt, nodes, depth, radius, ptArray, initstep = Math.random()) {
+  for (var i = countStart; i < nodes; i++) {
     ctx.moveTo(pt.x, pt.y)
-    var r = spread + Math.random() * spread * 0.7
-    var shift = i / nodes + Math.random() * 0.2
-    var x = r * Math.cos(2 * Math.PI * shift)
-    var y = r * Math.sin(2 * Math.PI * shift)
+    var step = initstep + (i / nodes) + 0.5 + (Math.random() * 0.2 - 0.1)
+    radius += radius * Math.random() * 0.15
+    var x = Math.cos(Math.PI * 2 * step) * radius
+    var y = Math.sin(Math.PI * 2 * step) * radius
     var nPt = new Point(pt.x + x, pt.y + y)
     ptArray.push(nPt)
     ctx.lineTo(nPt.x, nPt.y)
-
     if (depth > 0) {
-      createPointNode(ctx, nPt, Math.ceil(Math.random() * 5), depth - 1, spread * 0.7, ptArray)
+      createPointNode(1, ctx, nPt, nodes, depth - 1, radius * 0.7, ptArray, step)
     }
   }
-  depth--
 }
 
 function createNodeCircles (ctx, ptArray) {
   for (var i = 0; i < ptArray.length; i++) {
     var pt = ptArray[i]
     ctx.moveTo(pt.x, pt.y)
-    ctx.arc(pt.x, pt.y, 2, 0, 2 * Math.PI)
+    ctx.arc(pt.x, pt.y, 1.5, 0, 2 * Math.PI)
   }
 }
 
@@ -46,17 +44,23 @@ export default {
     const w = canvas.offsetWidth << 1
     const h = canvas.offsetHeight << 1
 
+    var colourSet = ['#E7A10E', '#F5D123']
+
     var ctx = canvas.getContext('2d')
     ctx.canvas.width = w
     ctx.canvas.height = h
 
     var gradient = ctx.createLinearGradient(0, 0, w, h)
-    gradient.addColorStop(0, '#E7A10E')
-    gradient.addColorStop(1, '#F5D123')
+    for (var i = 0; i < colourSet.length; i++) {
+      gradient.addColorStop(i, colourSet[i])
+    }
+
+    colourSet.reverse()
 
     var gradient2 = ctx.createLinearGradient(0, 0, w, h)
-    gradient2.addColorStop(0, '#F5D123')
-    gradient2.addColorStop(1, '#E7A10E')
+    for (i = 0; i < colourSet.length; i++) {
+      gradient2.addColorStop(i, colourSet[i])
+    }
 
     ctx.fillStyle = gradient
     ctx.fillRect(0, 0, w, h)
@@ -64,9 +68,10 @@ export default {
     var ptArray = [new Point(w >> 1, h >> 1)]
 
     ctx.beginPath()
-    var depth = 9
-    var spread = 1200
-    createPointNode(ctx, ptArray[0], 3, depth, spread, ptArray)
+    var depth = 14
+    var size = 500
+    var nodes = 3
+    createPointNode(0, ctx, ptArray[0], nodes, depth, size, ptArray)
     ctx.lineWidth = 1
     ctx.strokeStyle = gradient2
     ctx.stroke()
